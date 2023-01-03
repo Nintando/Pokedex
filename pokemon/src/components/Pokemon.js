2d85c916aab5cb5d7a74116ee867681baa96e544
hazem fad <hazemjuninho@yahoo.fr>
2023-01-03 19:46:41 +0100

    coin and design

--- pokemon/src/components/Pokemon.js
+++
@@ -1,9 +0,0 @@
-export default async function getPokemon(url) {
-  return new Promise((resolve, reject) => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        resolve(data);
-      });
-  });
-}
