e7470e482f025ffcabcd44d3c10131be40213960
Nintando <nintandot@gmail.com>
2023-01-04 10:46:47 +0100

    70% finish

--- pokemon/src/components/Cards/Card.js
+++ pokemon/src/components/Cards/Card.js
@@ -1,10 +1,10 @@
 import getPokedex from "../Pokedex";
-import "../../styles/pokeTypes.css";
-
+import "../../styles/pokeType.css";
 import { useState, useEffect } from "react";
 
 export default function Card({ pokemon }) {
   const [data, setData] = useState([]);
+  const [Stats, setStats] = useState(false);
   let name = pokemon.name;
   let types = pokemon.types;
   let url = pokemon.sprites.front_default;
@@ -15,51 +15,51 @@ export default function Card({ pokemon }) {
     });
   }, []);
 
+  const showStats = () => {
+    setStats(true);
+  };
+
+  const hideStats = () => {
+    setStats(false);
+  };
+
   const tab = [];
   // eslint-disable-next-line
   data.map((data) => {
     tab.push(data.pokeName);
   });
 
-  const putPokeToDB = async () => {
-    if (tab.includes(name)) {
-      return alert("existe deja");
-    } else {
-      fetch("http://localhost:5000/add", {
-        method: "POST",
-        body: JSON.stringify({
-          pokeName: name,
-          pokeTypes: types,
-          pokeImg: url,
-        }),
-        headers: { "Content-Type": "application/json" },
-      })
-        .then(alert("Vous avez ajouté le pokemon dans votre Pokédex"))
-        .then(window.location.reload(true))
-        .catch((err) => console.log(err));
-    }
-  };
-
   return (
-    <div className="Card" key={pokemon._id}>
+    <div
+      className="Card"
+      key={pokemon._id}
+      onMouseEnter={showStats}
+      onMouseLeave={hideStats}
+    >
       <div className="Card_Container">
+        <div># {pokemon.id}</div>
         <div className="Card_name">{pokemon.name}</div>
         <div className="Card_img">
           <img src={pokemon.sprites.front_default} alt="" /> <br />
         </div>
         {pokemon.types.map((type, i) => {
           return (
-            <span className={`type ${type.type.name}`} key={i}>
+            <div className={`type ${type.type.name} `} key={i}>
               {type.type.name}
-            </span>
+            </div>
           );
         })}
       </div>
-      <div className="btn">
-        <button className="btn-add" onClick={putPokeToDB}>
-          Ajouter au pokédex
-        </button>
-      </div>
+      {Stats && (
+        <div className="stats">
+          <p>PV: {pokemon.stats[5].base_stat}</p>
+          <p>Attaque: {pokemon.stats[4].base_stat}</p>
+          <p>Défense: {pokemon.stats[3].base_stat}</p>
+          <p>Defense speciale: {pokemon.stats[2].base_stat}</p>
+          <p>Attaque speciale: {pokemon.stats[1].base_stat}</p>
+          <p>Vitesse: {pokemon.stats[0].base_stat}</p>
+        </div>
+      )}
     </div>
   );
 }
